ec79770477b6124de4603db58bfd993d6e77c102
Hikigaya Houtarou <saifulfools@gmail.com>
2021-08-31 04:54:40 +0600

    modification in the procress

--- lib/screens/real-state-screen.dart
+++ lib/screens/real-state-screen.dart
@@ -17,60 +17,77 @@ class RealStateScreen extends StatefulWidget {
 }
 
 class _RealStateScreenState extends State<RealStateScreen> {
-  final FlutterTts tts = FlutterTts();
   final realStateService = RealStateService();
+  FlutterTts tts = new FlutterTts();
   StreamSubscription<Position> positionStream;
   Location location = Location();
+  Map staticGpsPos = {'lat': 0, 'long': 0};
+  List propertyList = [];
+  double distance = 0;
+  Position liveGpsPos;
 
   @override
   void initState() {
-    tts.setLanguage("en");
-    tts.setSpeechRate(0.4);
+    initFlutterTTS();
+    staticGpsPos = widget.gpsPosition;
+    getPropertyList();
     super.initState();
   }
 
   @override
   void dispose() {
-    tts.stop();
-    tts?.cancelHandler();
     positionStream?.cancel();
     super.dispose();
   }
 
-  Future getLiveRealStateList(context) async {
-    // positionStream =  Geolocator.getPositionStream(desiredAccuracy: LocationAccuracy.high, 
-    //   forceAndroidLocationManager: true).listen((Position position) {
-    //   var distance = Geolocator.distanceBetween(widget.gpsPosition['lat'], widget.gpsPosition['long'], position.latitude, position.longitude);
-    //   if(distance > 250.0) {
-    //     reFetchData(context);
-    //   } 
-    // });
-    return await realStateService.getRealStateList(latitude: widget.gpsPosition['lat'], longitude: widget.gpsPosition['long']);
+  initFlutterTTS() async {
+    tts.setLanguage("en");
+    tts.setSpeechRate(0.4);
+    await tts.speak(' ');
+  }
+
+  Future getPropertyList() async {
+    print('being called!');
+    var res =  await realStateService.getRealStateList(latitude: staticGpsPos['lat'], longitude: staticGpsPos['long']);
+    livePositionStream();
+    setState(() => propertyList = res);
+    if (propertyList.length > 0) await readAloud(propertyList);
+  }
+
+  livePositionStream() {
+    positionStream =  Geolocator.getPositionStream(desiredAccuracy: LocationAccuracy.high, 
+      forceAndroidLocationManager: true).listen((Position position) {
+      liveGpsPos = position;
+      distance = Geolocator.distanceBetween(staticGpsPos['lat'], staticGpsPos['long'], position.latitude, position.longitude);
+      if(distance > 250.0) {
+        distance = 0;
+        reFetchData();
+      }
+    });
   }
 
   readAloud(List data) async {
-    if(data.length > 1) {
+    if(data.length > 0) {
       String text = '${data.length} real state property found!';
       for(var item in data) {
-        text = text+ '${item['statusText']}!.'+item['address']+'.'+item['beds'].toString()+' Beds,'+
-        item['baths'].toInt().toString()+' Baths,'+'Area: '+item['area'].toString()+' square feets.\n'+
+        text = text+ '${item['statusText']}!.'+item['address']+'.\n'+item['beds'].toString()+' Beds,'+
+        item['baths'].toInt().toString()+' Baths,'+'Area: '+item['area'].toString()+' square feets.'+
         'Price'+ item['price']+'.\n';
       }
       await tts.speak(text);
     }
-  }
+  } 
 
-  reFetchData(context) async {
-    tts.stop();
-    positionStream?.cancel();
-    var res = await location.getCurrentLocation().catchError((e) => reStartApp()); 
-    if(res == 'failed') reStartApp();
-    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => RealStateScreen(gpsPosition: res)));
+  reFetchData() async {
+    tts?.stop();
+    // var res = await location.getCurrentLocation().catchError((e) => reStartApp());
+    reStartApp();
   }
 
   reStartApp() {
     Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoadingScreen()));
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -87,76 +104,70 @@ class _RealStateScreenState extends State<RealStateScreen> {
           IconButton(
             icon: Icon(Icons.near_me, color: Colors.white,), 
             onPressed: () {
-              reFetchData(context);
+              reStartApp();
             }
           ),
         ],
       ),
       body: SingleChildScrollView(
-        child: FutureBuilder(
-          future: getLiveRealStateList(context),
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            if(snapshot.hasData) readAloud(snapshot.data);
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(15.0),
-                  child: Text(
-                    'Nearby Real State found ${snapshot.data?.length ?? 0}', 
-                    style: TextStyle(
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.bold,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(15.0),
+              child: Text(
+                'Nearby Real State found ${propertyList?.length ?? 0}', 
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 15.0, bottom: 15.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(right: 10.0),
+                    child: Text(
+                      'Search Area Radius: $searchRadius feets', 
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(left: 15.0, bottom: 15.0),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.only(right: 10.0),
-                        child: Text(
-                          'Search Area Radius: $searchRadius feets', 
-                          style: TextStyle(
-                            fontSize: 14.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                      TextButton(
-                        style: TextButton.styleFrom(
-                          tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                          backgroundColor: Colors.teal,
-                          primary: Colors.white,
-                          padding: EdgeInsets.zero,
-                          minimumSize: Size(60, 25),
-                        ),
-                        onPressed: () {
-                          tts.stop();
-                          Navigator.push(context, MaterialPageRoute(builder: (context) => EditSearchRadiusScreen()))
-                          .then((value) {
-                            if(value == 'saved') {
-                              offset = realStateService.searchAreaOffset(searchRadius);
-                              reFetchData(context);
-                            }
-                          });
-                        }, 
-                        child: Text('Change', style: TextStyle(fontSize: 11.0, fontWeight: FontWeight.bold),),
-                      ),
-                    ],
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      backgroundColor: Colors.teal,
+                      primary: Colors.white,
+                      padding: EdgeInsets.zero,
+                      minimumSize: Size(60, 25),
+                    ),
+                    onPressed: () {
+                      tts.stop();
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => EditSearchRadiusScreen()))
+                      .then((value) {
+                        if(value == 'saved') {
+                          offset = realStateService.searchAreaOffset(searchRadius);
+                          reFetchData();
+                        }
+                      });
+                    }, 
+                    child: Text('Change', style: TextStyle(fontSize: 11.0, fontWeight: FontWeight.bold),),
                   ),
-                ),
-                for(var item in snapshot.data) Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: Text(item['statusText']+'\n'+item['address']+'\n'+
-                  'Beds: '+item['beds'].toString()+',    Baths: '+item['baths'].toString()+',    Area: '+item['area'].toString()+' sqft\n'+
-                  'Price'+ item['price']),
-                ),
-              ],
-            );
-          },
+                ],
+              ),
+            ),
+            for(var item in propertyList) Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Text(item['statusText']+'\n'+item['address']+'\n'+
+              'Beds: '+item['beds'].toString()+',    Baths: '+item['baths'].toInt().toString()+',    Area: '+item['area'].toString()+' sqft\n'+
+              'Price'+ item['price']),
+            ),
+          ],
         ),
       ),
     );
--- lib/services/real-state-service.dart
+++ lib/services/real-state-service.dart
@@ -10,8 +10,9 @@ class RealStateService {
         '$ZILLOW_IP?searchQueryState={"pagination":{},"mapBounds":{"west":${pos["west"]},"east":${pos["east"]},"south":${pos["south"]},"north":${pos["north"]}},"isMapVisible":true,"filterState":{"price":{"min":0},"monthlyPayment":{"min":0},"isAllHomes":{"value":true},"isManufactured":{"value":false},"isLotLand":{"value":false},"isCondo":{"value":false},"isApartmentOrCondo":{"value":false}},"isListVisible":true,"mapZoom":14}&wants={"cat1":["listResults","mapResults"],"cat2":["total"]}&requestId=8'
       ));
       return json.decode(res.body)['cat1']['searchResults']['listResults'];
-    } on FormatException catch(_) {
-      print('caught format exception!');
+    } on FormatException catch(e) {
+      print('Error: getRealStateList()\n$e');
+      return [];
     }
   }
 
