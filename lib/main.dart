c6aa44e3e0332ce9e1fc921e9e24a580267d3194
Hikigaya Houtarou <saifulfools@gmail.com>
2021-09-03 02:07:59 +0600

    prevent mobile from sleeping

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:real_state_finder/screens/loading-screen.dart';
+import 'package:wakelock/wakelock.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Wakelock.enable();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
--- lib/services/real-state-service.dart
+++ lib/services/real-state-service.dart
@@ -8,7 +8,7 @@ class RealStateService {
     try {
       var pos = exploreLatLong(position.latitude, position.longitude);
       var res = await http.get(Uri.parse(
-        '$ZILLOW_IP?searchQueryState={"pagination":{},"mapBounds":{"west":${pos["west"]},"east":${pos["east"]},"south":${pos["south"]},"north":${pos["north"]}},"isMapVisible":true,"filterState":{"price":{"min":0},"monthlyPayment":{"min":0},"isAllHomes":{"value":true},"isManufactured":{"value":false},"isLotLand":{"value":false},"isCondo":{"value":false},"isApartmentOrCondo":{"value":false}},"isListVisible":true,"mapZoom":14}&wants={"cat1":["listResults","mapResults"],"cat2":["total"]}&requestId=8'
+        '$ZILLOW_IP?searchQueryState={"pagination":{},"mapBounds":{"west":${pos["west"]},"east":${pos["east"]},"south":${pos["south"]},"north":${pos["north"]}},"isMapVisible":true,"filterState":{"price":{"min":0},"monthlyPayment":{"min":0},"isAllHomes":{"value":true},"isManufactured":{"value":false},"isLotLand":{"value":false},"isCondo":{"value":false},"isApartmentOrCondo":{"value":false}},"isListVisible":true,"mapZoom":12}&wants={"cat1":["listResults","mapResults"],"cat2":["total"]}&requestId=8'
       ));
       return json.decode(res.body)['cat1']['searchResults']['listResults'];
     } on FormatException catch(e) {
